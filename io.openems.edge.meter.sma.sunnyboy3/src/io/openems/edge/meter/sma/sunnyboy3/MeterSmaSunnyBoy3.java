b7a31e17e907099c2ae87ad38d4eba926ae6f1a2
Sven Hakvoort <sven.hakvoort@gmail.com>
2022-09-03 18:50:15 +0200

    Update dsmr

---
+++ io.openems.edge.meter.sma.sunnyboy3/src/io/openems/edge/meter/sma/sunnyboy3/MeterSmaSunnyBoy3.java
@@ -0,0 +1,26 @@
+package io.openems.edge.meter.sma.sunnyboy3;
+
+import io.openems.common.channel.Unit;
+import io.openems.common.types.OpenemsType;
+import io.openems.edge.common.channel.Doc;
+
+public interface MeterSmaSunnyBoy3 {
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		FREQUENCY_IN_HERTZ(Doc.of(OpenemsType.INTEGER)
+				.unit(Unit.HERTZ))
+		;
+
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+
+}
--- io.openems.edge.meter.sma.sunnyboy3/src/io/openems/edge/meter/sma/sunnyboy3/MeterSmaSunnyBoy3Impl.java
+++ io.openems.edge.meter.sma.sunnyboy3/src/io/openems/edge/meter/sma/sunnyboy3/MeterSmaSunnyBoy3Impl.java
@@ -1,121 +1,104 @@
 package io.openems.edge.meter.sma.sunnyboy3;
 
-import com.google.common.collect.ImmutableMap;
-import io.openems.common.channel.AccessMode;
 import io.openems.common.exceptions.OpenemsException;
+import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
 import io.openems.edge.bridge.modbus.api.BridgeModbus;
 import io.openems.edge.bridge.modbus.api.ModbusComponent;
-import io.openems.edge.bridge.modbus.sunspec.DefaultSunSpecModel;
-import io.openems.edge.bridge.modbus.sunspec.SunSpecModel;
+import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.element.SignedDoublewordElement;
+import io.openems.edge.bridge.modbus.api.element.UnsignedDoublewordElement;
+import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
+import io.openems.edge.common.channel.Channel;
+import io.openems.edge.common.channel.value.Value;
 import io.openems.edge.common.component.OpenemsComponent;
-import io.openems.edge.common.event.EdgeEventConstants;
-import io.openems.edge.common.modbusslave.ModbusSlave;
-import io.openems.edge.common.modbusslave.ModbusSlaveNatureTable;
-import io.openems.edge.common.modbusslave.ModbusSlaveTable;
 import io.openems.edge.common.taskmanager.Priority;
-import io.openems.edge.meter.api.AsymmetricMeter;
+import io.openems.edge.meter.api.MeterType;
 import io.openems.edge.meter.api.SymmetricMeter;
-import io.openems.edge.pvinverter.api.ManagedSymmetricPvInverter;
-import io.openems.edge.pvinverter.sunspec.AbstractSunSpecPvInverter;
-import io.openems.edge.pvinverter.sunspec.SunSpecPvInverter;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.ComponentContext;
-import org.osgi.service.component.annotations.*;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventHandler;
-import org.osgi.service.event.propertytypes.EventTopics;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.metatype.annotations.Designate;
 
-import java.util.Map;
+import java.util.function.Consumer;
 
 @Designate(ocd = Config.class, factory = true)
 @Component(
-        name = "Meter.SMA.sunnyboy3",
-        immediate = true,
-        configurationPolicy = ConfigurationPolicy.REQUIRE,
-        property = {
-                "type=PRODUCTION"
-        })
-@EventTopics({
-        EdgeEventConstants.TOPIC_CYCLE_EXECUTE_WRITE
-})
-public class MeterSmaSunnyBoy3Impl extends AbstractSunSpecPvInverter implements SunSpecPvInverter, ManagedSymmetricPvInverter,
-        SymmetricMeter, ModbusComponent, AsymmetricMeter, OpenemsComponent, EventHandler, ModbusSlave {
+		name = "Meter.SMA.sunnyboy3",
+		immediate = true,
+		configurationPolicy = ConfigurationPolicy.REQUIRE
+)
+public class MeterSmaSunnyBoy3Impl extends AbstractOpenemsModbusComponent implements SymmetricMeter, ModbusComponent, OpenemsComponent, MeterSmaSunnyBoy3 {
 
-    private static final Map<SunSpecModel, Priority> ACTIVE_MODELS = ImmutableMap.<SunSpecModel, Priority>builder()
-            .put(DefaultSunSpecModel.S_1, Priority.LOW) // from 40002
-            .put(DefaultSunSpecModel.S_101, Priority.LOW) // from 40081
-            .put(DefaultSunSpecModel.S_103, Priority.HIGH) // from 40185
-            .put(DefaultSunSpecModel.S_120, Priority.LOW) // from 40237
-            .put(DefaultSunSpecModel.S_121, Priority.LOW) // from 40265
-            .put(DefaultSunSpecModel.S_122, Priority.LOW) // from 40297
-            .put(DefaultSunSpecModel.S_123, Priority.LOW) // from 40343
-            .build();
+	private MeterType meterType = MeterType.PRODUCTION;
 
-    // Further available SunSpec blocks provided by SMA Sunny TriPower are:
-    // .put(DefaultSunSpecModel.S_11, Priority.LOW) // from 40070
-    // .put(DefaultSunSpecModel.S_12, Priority.LOW) // from 40085
-    // .put(DefaultSunSpecModel.S_124, Priority.LOW) // from 40369
-    // .put(DefaultSunSpecModel.S_126, Priority.LOW) // from 40395
-    // .put(DefaultSunSpecModel.S_127, Priority.LOW) // from 40461
-    // .put(DefaultSunSpecModel.S_128, Priority.LOW) // from 40473
-    // .put(DefaultSunSpecModel.S_131, Priority.LOW) // from 40489
-    // .put(DefaultSunSpecModel.S_132, Priority.LOW) // from 40555
-    // .put(DefaultSunSpecModel.S_160, Priority.LOW) // from 40621
-    // .put(DefaultSunSpecModel.S_129, Priority.LOW) // from 40751
-    // .put(DefaultSunSpecModel.S_130, Priority.LOW) // from 40813
+	@Reference
+	protected ConfigurationAdmin cm;
 
-    private static final int READ_FROM_MODBUS_BLOCK = 1;
+	public MeterSmaSunnyBoy3Impl() {
+		super(
+				OpenemsComponent.ChannelId.values(),
+				ModbusComponent.ChannelId.values(),
+				SymmetricMeter.ChannelId.values(),
+				MeterSmaSunnyBoy3.ChannelId.values()
+		);
+	}
 
-    @Reference
-    protected ConfigurationAdmin cm;
+	@Override
+	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+	protected void setModbus(BridgeModbus modbus) {
+		super.setModbus(modbus);
+	}
 
-    public MeterSmaSunnyBoy3Impl() throws OpenemsException {
-        super(//
-                ACTIVE_MODELS, //
-                OpenemsComponent.ChannelId.values(), //
-                ModbusComponent.ChannelId.values(), //
-                SymmetricMeter.ChannelId.values(), //
-                AsymmetricMeter.ChannelId.values(), //
-                ManagedSymmetricPvInverter.ChannelId.values(), //
-                SunSpecPvInverter.ChannelId.values() //
-        );
-    }
+	@Activate
+	void activate(ComponentContext context, Config config) throws OpenemsException {
+		this.meterType = config.type();
+		super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm,
+				"Modbus", config.modbus_id());
+	}
 
-    @Override
-    @Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
-    protected void setModbus(BridgeModbus modbus) {
-        super.setModbus(modbus);
-    }
+	@Override
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
 
-    @Activate
-    void activate(ComponentContext context, Config config) throws OpenemsException {
-        if (super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm,
-                "Modbus", config.modbus_id(), READ_FROM_MODBUS_BLOCK, config.phase())) {
-            return;
-        }
-    }
+	@Override
+	public MeterType getMeterType() {
+		return this.meterType;
+	}
 
-    @Override
-    @Deactivate
-    protected void deactivate() {
-        super.deactivate();
-    }
+	@Override
+	protected ModbusProtocol defineModbusProtocol() throws OpenemsException {
+		var modbusProtocol = new ModbusProtocol(this,
+				new FC3ReadRegistersTask(30775, Priority.HIGH, m(SymmetricMeter.ChannelId.ACTIVE_POWER, new SignedDoublewordElement(30775))),
+				new FC3ReadRegistersTask(30803, Priority.HIGH, m(MeterSmaSunnyBoy3.ChannelId.FREQUENCY_IN_HERTZ, new UnsignedDoublewordElement(30803))),
+				new FC3ReadRegistersTask(30805, Priority.HIGH, m(SymmetricMeter.ChannelId.REACTIVE_POWER, new SignedDoublewordElement(30805)))
+		);
 
-    @Override
-    public void handleEvent(Event event) {
-        super.handleEvent(event);
-    }
+		this.addCalculateChannelListeners();
 
-    @Override
-    public ModbusSlaveTable getModbusSlaveTable(AccessMode accessMode) {
-        return new ModbusSlaveTable(//
-                OpenemsComponent.getModbusSlaveNatureTable(accessMode), //
-                SymmetricMeter.getModbusSlaveNatureTable(accessMode), //
-                ManagedSymmetricPvInverter.getModbusSlaveNatureTable(accessMode), //
-                ModbusSlaveNatureTable.of(MeterSmaSunnyBoy3Impl.class, accessMode, 100) //
-                        .build());
-    }
+		return modbusProtocol;
+	}
+
+	private void addCalculateChannelListeners() {
+		Channel<Long> frequencyChannelInHertz = this.channel(MeterSmaSunnyBoy3.ChannelId.FREQUENCY_IN_HERTZ);
+		final Consumer<Value<Long>> convertHertzToMilliHertz = value -> {
+			this.getFrequencyChannel().setNextValue(value.get().intValue() / 1000);
+		};
+
+		frequencyChannelInHertz.onSetNextValue(convertHertzToMilliHertz);
+	}
+
+	@Override
+	public String debugLog() {
+		return "L:" + this.getActivePower().asString();
+	}
 
 }
-
