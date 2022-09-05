4faa41c1155aa897c6eae60535a5f3f567ba7b91
Sven Hakvoort <sven.hakvoort@gmail.com>
2022-09-05 10:32:35 +0200

    Add esmr module

--- io.openems.edge.bridge.esmr/src/io/openems/edge/bridge/esmr/api/EsmrTask.java
+++ io.openems.edge.bridge.esmr/src/io/openems/edge/bridge/esmr/api/EsmrTask.java
@@ -1,12 +1,6 @@
 package io.openems.edge.bridge.esmr.api;
 
-import gnu.io.*;
-import io.openems.edge.bridge.esmr.util.ReadUTF8RecordStream;
 import io.openems.edge.common.component.AbstractOpenemsComponent;
-import org.openmuc.jmbus.VariableDataStructure;
-
-import java.io.*;
-import java.util.TooManyListenersException;
 
 public class EsmrTask {
 
