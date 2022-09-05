f4daa6fd09dcd59220925ca45a67d22bdfe614c5
Sven Hakvoort <sven.hakvoort@gmail.com>
2022-09-05 20:39:27 +0200

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,7 @@
+VOLUMES=("openems_chronografStorage" "openems_edgeData" "openems_pgdata" "openems_influxData" "openems_influxConfig" "openems_grafanaStorage" "openems_chronografStorage")
+
+mkdir ~/backups
+for volume in "${VOLUMES[@]}"
+do
+  docker run --rm -v "$volume:/volume" -v "$HOME/backups:/backup" ubuntu tar cvf "/backup/$volume-backup.tar" /volume
+done
